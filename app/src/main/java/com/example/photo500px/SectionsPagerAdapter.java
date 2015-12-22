2e418e43e98f3664a31c6903f3adca27135097d3
Aleshin Igor <i.aleshin@rambler-co.ru>
2015-12-22 18:20:37 +0300

    add restoring fragments after long-long time

--- app/src/main/java/com/example/photo500px/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/photo500px/SectionsPagerAdapter.java
@@ -1,5 +1,7 @@
 package com.example.photo500px;
 
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -15,12 +17,16 @@ import rx.functions.Action1;
  */
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
+    private final String FRAGMENTS_UNIQUE_SAVING_KEY = "FRAGMENTS_UNIQUE_SAVING_KEY";
+
     private List<Fragment> fragmentList = new ArrayList<>();
     private List<String> titlesList = new ArrayList<>();
     private int currentPosition = 0;
+    private FragmentManager fm;
 
     public SectionsPagerAdapter(FragmentManager fm) {
         super(fm);
+        this.fm = fm;
     }
 
     public void addFragment(MainTabFragment fragment, String title) {
@@ -31,6 +37,31 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public Fragment getItem(int position) {
         return fragmentList.get(position);
+    }
+
+    @Override
+    public void restoreState(Parcelable state, ClassLoader loader) {
+        super.restoreState(state, loader);
+        Bundle bundle = (Bundle) state;
+        fragmentList.clear();
+        for (String key : bundle.keySet()) {
+            if (key.startsWith(FRAGMENTS_UNIQUE_SAVING_KEY)) {
+                Fragment f = fm.getFragment(bundle, key);
+                fragmentList.add(f);
+            }
+        }
+    }
+
+    @Override
+    public Parcelable saveState() {
+        Bundle state = (Bundle) super.saveState();
+        if (state == null)
+            state = new Bundle();
+
+        for (int i = 0; i < fragmentList.size(); i++) {
+            fm.putFragment(state, FRAGMENTS_UNIQUE_SAVING_KEY + i, fragmentList.get(i));
+        }
+        return state;
     }
 
     @Override
